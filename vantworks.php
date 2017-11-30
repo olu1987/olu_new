7979721a9c469fd262dad7fdf6a3de6fcd5b30f7
Olu Meduoye <olu.meduoye@tvplayer.com>
2017-11-30 18:02:55 +0000

    fixes for vantworks page

--- vantworks.php
+++ vantworks.php
@@ -8,14 +8,22 @@
                 <h1 class="task-main-header">Vantworks</h1>
             </div>
         </div>
-        <div class="row">
+        <div class="row mb-50 text-center">
+            <div class="col-md-8 col-md-offset-2">
+                <h2 class="text-uppercase">the challenge</h2>
+                <p>"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I
+                    will give you a complete account of the system, and expound the actual teachings of the great explorer of
+                    "</p>
+            </div>
+        </div>
+        <div class="row vantworks-promo-top">
             <div class="col-sm-8 col-sm-offset-2">
                 <img class="img-responsive" src="img/vantworks-lander-1.jpg"/>
             </div>
         </div>
         <div class="row task-description">
             <div class="col-md-8 col-md-offset-2">
-                <h2 class="text-uppercase">the task</h2>
+                <h2 class="text-uppercase">the approach</h2>
                 <p>"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I
                     will give you a complete account of the system, and expound the actual teachings of the great explorer of
                     the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself,
@@ -24,26 +32,12 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-sm-8 col-sm-offset-2">
+            <div class="col-md-8 col-md-offset-2">
                 <img class="img-responsive" src="img/vantworks-lander-2.jpg"/>
             </div>
         </div>
     </div>
     <div class="container vantworks-bg">
-        <div class="row task-description">
-            <div class="col-md-8 col-md-offset-2">
-                <h2 class="text-uppercase">my approach</h2>
-                <p>"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I
-                    will give you a complete account of the system, and expound the actual teachings of the great explorer of
-                    the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself,
-                    because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter
-                    consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to
-                    obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can
-                    procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise,
-                    except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy
-                    a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"</p>
-            </div>
-        </div>
         <div class="row portfolio">
             <div class="col-md-12">
                 <div class="tile col-md-4 col-sm-12 tech">
